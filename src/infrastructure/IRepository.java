3a2be9e62ac46f15125e9eade59f2ea9ef1fcd58
Cristian Stanciu <crististanciu095@gmail.com>
2022-11-02 21:12:55 +0200

    Added specifications to the functions/methods from infrastructure (repository) package (layer)

--- src/infrastructure/IRepository.java
+++ src/infrastructure/IRepository.java
@@ -4,15 +4,47 @@ import domain.Entity;
 import exception.RepoException;
 
 public interface IRepository<ID, E extends Entity<ID>> {
+    /**
+     * Metoda publica abstracta (nedefinita) de tip void (nu returneaza/intoarce nicio valoare) care adauga o entitate entity de tipul E in repozitoriu
+     * @param entity - obiect de clasa E (entitatea pe care dorim sa o adaugam in repozitoriu, adica in reteaua de socializare)
+     * @throws RepoException - exceptie aruncata daca entitatea entity exista deja in repozitoriu (reteaua de socializare); in retea nu putem avea entitati duplicate
+     */
     void add(E entity) throws RepoException;
 
+    /**
+     * Metoda publica abstracta (nedefinita) de tipul E (intoarce/returneaza un obiect de clasa/tipul E) care sterge un obiect cu id-ul id din repozitoriu (reteaua de socializare)
+     * @param id - obiect de clasa/tipul ID (id-ul obiectului pe care dorim sa il stergem din retea (repozitoriu))
+     * @return - metoda intoarce/returneaza obiectul sters (daca stergerea se realizeaza cu succes)
+     * @throws RepoException - exceptie aruncata daca nu exista nicio entitate cu id-ul id in repozitoriu (adica in reteaua de socializare)
+     */
     E remove(ID id) throws RepoException;
 
+    /**
+     * Metoda publica abstracta (nedefinita) de tipul E (tip generic de date) care modifica/actualizeaza un de tipul E din retea (repozitoriu)
+     * @param entity - obiect de tipul E (tip generic) pe care dorim sa il modificam/actualizam
+     * @return - entitatea modificata/actualizata (obiect de clasa E)
+     * @throws RepoException - exceptie aruncata daca nu exista entitati in reteaua de socializare sau daca entitatea pe care dorim sa o modificam nu exista in retea (repozitoriu)
+     */
     E modify(E entity) throws RepoException;
 
+    /**
+     * Metoda publica abstracta (nedefinita) de tipul E (returneaza/intoarce un obiect de tipul E (tip generic)) care cauta o entitate dupa id in retea
+     * @param id - obiect de tipul ID (tip generic de date) reprezentand id-ul entitatii pe care dorim sa o cautam in retea (repozitoriu)
+     * @return - entitatea cu id-ul id (daca aceasta exista in retea/repozitoriu)
+     * @throws RepoException - exceptie aruncata daca nu exista nicio entitate cu id-ul id in reteaua de socializare (repozitoriu) sau daca nu exista entitati in retea
+     */
     E search(ID id) throws RepoException;
 
+    /**
+     * Metoda publica abstracta (nedefinita) de tipul int (integer = intreg) care returneaza/intoarce numarul total de entitati din reteaua de socializare (repozitoriu)
+     * @return - numarul de entitati din retea (repozitoriu), valoare numerica intreaga cu semn (signed) pe 4 bytes/octeti (32 de biti) care este pozitiva (mai mare sau egala cu 0)
+     */
     int len();
 
+    /**
+     * Metoda publica abstracta (nedefinita) de tipul Iterable cu elemente de tipul E (entitati) care returneaza/intoarce o lista (obiect iterabil (care poate sa fie parcurs/iterat)) cu toate entitatile din retea (repozitoriu)
+     * @return - lista cu elemente de tipul E (E tip generic de date) care reprezinta toate entitatile din reteaua de socializare
+     * @throws RepoException - exceptie aruncata daca nu exista entitati in retea/repozitoriu
+     */
     Iterable<E> getAll() throws RepoException;
 }
--- src/infrastructure/memory/InMemoryRepo.java
+++ src/infrastructure/memory/InMemoryRepo.java
@@ -14,6 +14,11 @@ public class InMemoryRepo<ID, E extends Entity<ID>> implements IRepository<ID, E
         entities = new HashMap<>();
     }
 
+    /**
+     * Metoda publica definita de tipul void (functie procedurala care nu returneaza/intoarce nicio valoare) care adauga un obiect entity de tipul E in retea
+     * @param entity - obiect de tipul/clasa E pe care dorim sa il adaugam in retea
+     * @throws RepoException - exceptie aruncata daca exista deja un obiect cu acelasi id ca si entitatea entity pe care dorim sa o adaugam in retea
+     */
     @Override
     public void add(E entity) throws RepoException {
         if(entity == null) {
@@ -36,6 +41,12 @@ public class InMemoryRepo<ID, E extends Entity<ID>> implements IRepository<ID, E
         entities.put(entity.getId(), entity);
     }
 
+    /**
+     * Metoda publica definita de tipul E (returneaza/intoarce un obiect de clasa E) care sterge o entitate cu id-ul id din retea
+     * @param id - obiect de tipul ID (tip generic de reprezentare a datelor) reprezentand id-ul entitatii pe care dorim sa o stergem/eliminam din retea
+     * @return - obiect de tipul E (daca exista un obiect in retea (repozitoriu) care sa aiba id-ul egal cu parametrul id) reprezentand obiectul sters din reteaua de socializare
+     * @throws RepoException - exceptie aruncata in cazul in care nu exista nicio entitate in retea (repozitoriu) sau daca nu exista nicio entitate care sa aiba id-ul egal cu parametrul formal/simbolic id de tipul ID
+     */
     @Override
     public E remove(ID id) throws RepoException {
         if(id == null) {
@@ -54,6 +65,12 @@ public class InMemoryRepo<ID, E extends Entity<ID>> implements IRepository<ID, E
         return removedEntity;
     }
 
+    /**
+     * Metoda publica definita de tipul E (returneaza/intoarce un obiect de clasa E) care modifica un obiect entity din reteaua de socializare (daca acesta exista) sau arunca exceptie in cazul in care nu exista
+     * @param entity - obiect de tipul E (tip generic) pe care dorim sa il modificam/actualizam
+     * @return - obiect de tipul/clasa E reprezentand obiectul modificat/actualizat (daca modificarea se realizeaza cu succes (exista un obiect in retea care are acelasi id ca si entitatea entity))
+     * @throws RepoException - exceptie aruncata daca nu exista entitati in retea (repozitoriu) sau daca nu exista o entitate care sa aiba acelasi id ca si entity
+     */
     @Override
     public E modify(E entity) throws RepoException {
         if(entity == null) {
@@ -73,6 +90,12 @@ public class InMemoryRepo<ID, E extends Entity<ID>> implements IRepository<ID, E
         return modifiedEntity;
     }
 
+    /**
+     * Metoda publica definita de tipul E (intoarce/returneaza un obiect de tipul ID) care cauta o entitate in retea dupa id (cauta entitatea care are id-ul id)
+     * @param id - obiect de tipul ID care reprezinta id-ul entitatii pe care dorim sa o cautam
+     * @return - obiect de tipul E (tip de data generic) reprezentand id-ul obiectului pe care il cautam (daca acesta exista (altfel se arunca exceptie))
+     * @throws RepoException - exceptie aruncata daca nu exista entitati in retea sau daca nu exista nicio entitate cu id-ul id
+     */
     @Override
     public E search(ID id) throws RepoException {
         if(id == null) {
@@ -91,11 +114,20 @@ public class InMemoryRepo<ID, E extends Entity<ID>> implements IRepository<ID, E
         return searchedEntity;
     }
 
+    /**
+     * Metoda publica definita care returneaza/intoarce un numar intreg (valoare numerica intreaga) reprezentand numarul de entitati din retea (repozitoriu)
+     * @return - numar intreg pozitiv (mai mare sau egal cu 0) care reprezinta numarul total de entitati din reteaua de socializare
+     */
     @Override
     public int len() {
         return entities.values().size();
     }
 
+    /**
+     * Metoda publica definita care returneaza/intoarce toate entitatile de tipul E din retea (repozitoriu)
+     * @return - obict iterabil (lista, multime, etc.) care contine elemente de tipul E (entitati) reprezentand lista de entitati din reteaua de socializare
+     * @throws RepoException - exceptie aruncata daca nu exista entitati in retea
+     */
     @Override
     public Iterable<E> getAll() throws RepoException {
         if(entities.isEmpty()) {
