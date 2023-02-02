87f773ca9e7f3437bc9084c5e5b4a6c7b87936c3
Cristian Stanciu <crististanciu095@gmail.com>
2023-02-02 13:27:18 +0200

    Updated Main.java

--- src/Main.java
+++ src/Main.java
@@ -22,28 +22,11 @@ import utils.Constants;
 import presentation.UI;
 import test.ApplicationTester;
 
-import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-
-import org.jetbrains.annotations.NotNull;
-
 import java.io.File;
 import java.util.Scanner;
 import java.io.IOException;
 
-public class Main extends Application {
-    @Override
-    public void start(@NotNull Stage stage) throws IOException {
-        stage.setResizable(false);
-        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("logIn.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Welcome to Social Network!");
-        stage.setScene(scene);
-        stage.show();
-    }
-
+public class Main {
     public static void main(String[] args) {
         // TODO: update documentation (write specifications for the new methods/functions implemented)
         // TODO: update documentation (rewrite specifications for modified methods/functions)
@@ -58,60 +41,54 @@ public class Main extends Application {
         ApplicationTester applicationTester = new ApplicationTester();
         applicationTester.runAllTests();
 
-        boolean runGUI = true;
-        if(runGUI) {
-            launch();
-        }
-        else {
-            System.out.println("Choose how to save the entities (data persistence):");
-            System.out.println("[1] - in memory (in RAM - Random Access Memory)");
-            System.out.println("[2] - in text files (in CSV - Comma Separated Values files)");
-            System.out.println("[3] - in database (in SQL - Structured Query Language database)");
+        System.out.println("Choose how to save the entities (data persistence):");
+        System.out.println("[1] - in memory (in RAM - Random Access Memory)");
+        System.out.println("[2] - in text files (in CSV - Comma Separated Values files)");
+        System.out.println("[3] - in database (in SQL - Structured Query Language database)");
 
-            IRepository<Long, User> userRepo = new InMemoryRepo<>();
-            IRepository<Long, Friendship> friendshipRepo = new InMemoryRepo<>();
+        IRepository<Long, User> userRepo = new InMemoryRepo<>();
+        IRepository<Long, Friendship> friendshipRepo = new InMemoryRepo<>();
 
-            Scanner scanner = new Scanner(System.in);
-            boolean keepRunning = true;
-            while (keepRunning) {
-                keepRunning = false;
-                System.out.print("\n>>>");
-                String cmd = scanner.nextLine();
-                switch (cmd) {
-                    case "1":
-                        break;
-                    case "2":
-                        try {
-                            userRepo = new UserFileRepo(Constants.USER_TEXT_FILE_PATH);
-                            friendshipRepo = new FriendshipFileRepo(Constants.FRIENDSHIP_TEXT_FILE_PATH, userRepo);
-                        } catch (RepoException | IOException ex) {
-                            System.out.println("Current directory/folder is: " + new File(".").getAbsoluteFile());
-                            ex.printStackTrace();
-                            System.exit(1);
-                        }
-                        break;
-                    case "3":
-                        userRepo = new UserDbRepository(Constants.DATABASE_URL, Constants.DATABASE_USER, Constants.DATABASE_PASSWORD);
-                        friendshipRepo = new FriendshipDbRepository(Constants.DATABASE_URL, Constants.DATABASE_USER, Constants.DATABASE_PASSWORD, userRepo);
-                        break;
-                    default:
-                        System.out.print(ConsoleColors.RED + "[!]Invalid option!" + ConsoleColors.RESET);
-                        keepRunning = true;
-                }
-                System.out.println();
+        Scanner scanner = new Scanner(System.in);
+        boolean keepRunning = true;
+        while (keepRunning) {
+            keepRunning = false;
+            System.out.print("\n>>>");
+            String cmd = scanner.nextLine();
+            switch (cmd) {
+                case "1":
+                    break;
+                case "2":
+                    try {
+                        userRepo = new UserFileRepo(Constants.USER_TEXT_FILE_PATH);
+                        friendshipRepo = new FriendshipFileRepo(Constants.FRIENDSHIP_TEXT_FILE_PATH, userRepo);
+                    } catch (RepoException | IOException ex) {
+                        System.out.println("Current directory/folder is: " + new File(".").getAbsoluteFile());
+                        ex.printStackTrace();
+                        System.exit(1);
+                    }
+                    break;
+                case "3":
+                    userRepo = new UserDbRepository(Constants.DATABASE_URL, Constants.DATABASE_USER, Constants.DATABASE_PASSWORD);
+                    friendshipRepo = new FriendshipDbRepository(Constants.DATABASE_URL, Constants.DATABASE_USER, Constants.DATABASE_PASSWORD, userRepo);
+                    break;
+                default:
+                    System.out.print(ConsoleColors.RED + "[!]Invalid option!" + ConsoleColors.RESET);
+                    keepRunning = true;
             }
-
-            IValidator<Address> addressValidator = new AddressValidator();
-            IValidator<Credential> credentialValidator = new CredentialValidator();
-            IValidator<User> userValidator = new UserValidator(addressValidator, credentialValidator);
-            UserService userService = new UserService(userValidator, userRepo, friendshipRepo);
-
-            IValidator<Friendship> friendshipValidator = new FriendshipValidator();
-            FriendshipService friendshipService = new FriendshipService(friendshipValidator, friendshipRepo, userRepo);
-
-            SuperService superService = new SuperService(userService, friendshipService);
-            UI ui = new UI(superService);
-            ui.run();
+            System.out.println();
         }
+
+        IValidator<Address> addressValidator = new AddressValidator();
+        IValidator<Credential> credentialValidator = new CredentialValidator();
+        IValidator<User> userValidator = new UserValidator(addressValidator, credentialValidator);
+        UserService userService = new UserService(userValidator, userRepo, friendshipRepo);
+
+        IValidator<Friendship> friendshipValidator = new FriendshipValidator();
+        FriendshipService friendshipService = new FriendshipService(friendshipValidator, friendshipRepo, userRepo);
+
+        SuperService superService = new SuperService(userService, friendshipService);
+        UI ui = new UI(superService);
+        ui.run();
     }
 }
